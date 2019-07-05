4bb0d3fdc9998fccc32610c6e6ca7c665cab9c9c
zareen <zareentazshukur@gmail.com>
2019-07-04 23:13:58 -0500

    adding files

---
+++ Project/config.py
@@ -0,0 +1,4 @@
+# Enter your API keys
+yelp_api_key = "j9wKdC36yYmXpKbBr6cvC2KcCvx4m9jzUj_LWWVk5OaR-53vJQZ0Cdac2DjfK6ZF6SW0oCdbtzXrNrvuWC-gQSVFlP-rouLRe3Pfqi5-T39XmZ2TXLH-IVp9jxwUXXYx"
+
+open_weather_api_key = "aab3ee6dfef02dc1735226ee59218ce0"
